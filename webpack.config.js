fba53c8d17a8ad25e017fac3f13b78cfa90071e6
Dipper <1023128147@qq.com>
2022-07-29 20:54:48 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "XDML",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
